765c2651357e5116937b218aec58cb1f195b5dd7
Monirul Islam <mailmonir@gmail.com>
2023-10-18 07:03:15 +0600

    auth/callback redirect updated

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,16 +1,15 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export async function GET(req) {
-  const cookieStore = cookies()
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
-  const { searchParams } = new URL(req.url)
-  const code = searchParams.get('code')
+export async function GET(request) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    await supabase.auth.exchangeCodeForSession(code)
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+    await supabase.auth.exchangeCodeForSession(code);
   }
-
-  return NextResponse.redirect(new URL('/admin', req.url))
+  return NextResponse.redirect(requestUrl.origin + "/admin");
 }
