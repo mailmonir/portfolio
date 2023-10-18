a1df20301e90b10d1a2b681a5ebeb6c629b53bd2
Monirul Islam <mailmonir@gmail.com>
2023-10-18 10:04:30 +0600

    axios removed from recaptcha verify

--- app/lib/verifyRecaptcha.js
+++ app/lib/verifyRecaptcha.js
@@ -1,10 +1,9 @@
 "use server";
 
-import axios from "axios";
-
 export async function verifyCaptchaAction(token) {
-  const res = await axios.post(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
+  const res = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+    { method: "POST", headers: { "content-Type": "application/json" } }
   );
   if (res.data.score > 0.5) {
     return true;
